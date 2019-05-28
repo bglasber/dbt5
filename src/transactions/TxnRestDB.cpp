7dbcf2fef8f44b55d3901a2afdad97d096712ac6
Brad Glasbergen <bglasber@gmail.com>
2019-05-27 21:56:16 -0400

    Try simple change

--- src/transactions/TxnRestDB.cpp
+++ src/transactions/TxnRestDB.cpp
@@ -2101,14 +2101,14 @@ void TxnRestDB::execute( int clientId, const TTradeResultFrame2Input *pIn,
                     osSQL.clear();
                     osSQL.str("");
                     osSQL << "SELECT h_t_id, h_qty, h_price FROM holding ";
-                    osSQL << "WHERE h_ca_id = " << pIn->acct_id << " AND hs_s_symb = '";
+                    osSQL << "WHERE h_ca_id = " << pIn->acct_id << " AND h_s_symb = '";
                     osSQL << pIn->symbol << "' ORDER BY h_dts DESC";
                     hArr = sendQuery( clientId, osSQL.str().c_str() );
                 } else { //is_lifo
                     osSQL.clear();
                     osSQL.str("");
                     osSQL << "SELECT h_t_id, h_qty, h_price FROM holding ";
-                    osSQL << "WHERE h_ca_id = " << pIn->acct_id << " AND hs_s_symb = '";
+                    osSQL << "WHERE h_ca_id = " << pIn->acct_id << " AND h_s_symb = '";
                     osSQL << pIn->symbol << "' ORDER BY h_dts ASC";
                     hArr = sendQuery( clientId, osSQL.str().c_str() );
                 } //!is_lifo
@@ -2375,6 +2375,14 @@ void TxnRestDB::execute( int clientId, const TTradeStatusFrame1Input *pIn,
 	cout << "Trade_Status Frame 1 Done" << endl;
 }
 
+bool replace(std::string& str, const std::string& from, const std::string& to) {
+    size_t start_pos = str.find(from);
+    if(start_pos == std::string::npos)
+        return false;
+    str.replace(start_pos, from.length(), to);
+    return true;
+}
+
 void TxnRestDB::execute( int clientId, const TTradeUpdateFrame1Input *pIn,
                          TTradeUpdateFrame1Output *pOut ) {
 	cout << "Trade_Update Frame 1 Starting" << endl;
@@ -2396,14 +2404,12 @@ void TxnRestDB::execute( int clientId, const TTradeUpdateFrame1Input *pIn,
             string exec_name = tArr->at(0)->get("t_exec_name", "").asString();
             osSQL.clear();
             osSQL.str("");
+
+			
             if( exec_name.find(" X ") != string::npos ) {
-                osSQL << "SELECT REPLACE( '" << exec_name << "', ' X ', ' ' ) AS rep";
-                std::vector<Json::Value *> *rep = sendQuery( clientId, osSQL.str().c_str() );
-                exec_name = rep->at(0)->get("rep", "").asString();
+				replace( exec_name, " X ", " " );
             } else {
-                osSQL << "SELECT REPLACE( '" << exec_name << "', ' ', ' X ' ) AS rep";
-                std::vector<Json::Value *> *rep = sendQuery( clientId, osSQL.str().c_str() );
-                exec_name = rep->at(0)->get("rep", "").asString();
+				replace( exec_name, "  ", " X " );
             } //else
 
             osSQL.clear();
